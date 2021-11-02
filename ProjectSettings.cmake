866af230838f3fa03307a750a3e014734a2fc553
Piotr Nikiel <Piotr.Nikiel@cern.ch>
2021-11-02 16:39:27 +0100

    Added CANmodule, upgraded quasar

--- ProjectSettings.cmake
+++ ProjectSettings.cmake
@@ -3,7 +3,7 @@
 ## information how to use this file.
 ## ----------------------------------------------------
 
-set(CUSTOM_SERVER_MODULES )
+set(CUSTOM_SERVER_MODULES CanModule )
 set(EXECUTABLE OpcUaServer)
 set(SERVER_INCLUDE_DIRECTORIES  )
 set(SERVER_LINK_LIBRARIES  )
@@ -19,4 +19,6 @@ set(BUILD_SERVER_SHARED_LIB OFF)
 ## examples: date_time atomic etc.
 ## Note: boost paths are resolved either from $BOOST_ROOT if defined or system paths as fallback
 ##
-set(ADDITIONAL_BOOST_LIBS )
+set(ADDITIONAL_BOOST_LIBS filesystem )
+
+add_definitions(-fPIC)
