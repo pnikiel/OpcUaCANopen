1993923b6b211ab2dd631d0fe776fff47d84cefc
Piotr Nikiel <Piotr.Nikiel@cern.ch>
2022-02-11 19:21:18 +0100

    missing files

---
+++ Enumerator/generate.py
@@ -0,0 +1,27 @@
+import os
+import sys
+import argparse
+
+quasar_path = os.path.abspath(os.path.join(
+    os.path.dirname(__file__), os.pardir))
+
+sys.path.insert(0, os.path.join(quasar_path, 'FrameworkInternals'))
+
+from transformDesign import transformDesign
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--output_dir', default=quasar_path)
+
+args = parser.parse_args()
+
+transformDesign(
+    transform_path='Enumerator/templates/designToEnumeratorBody.jinja',
+    outputFile=os.path.join(args.output_dir, 'src', 'Enumerator.cpp'),
+    requiresMerge=False,
+    astyleRun=True)
+
+transformDesign(
+    transform_path='Enumerator/templates/designToEnumeratorHeader.jinja',
+    outputFile=os.path.join(args.output_dir, 'include', 'Enumerator.hpp'),
+    requiresMerge=False,
+    astyleRun=True)
